ba3f38efe86ae2692860275dc244f0f0ecffcef3
Tarek Aziz <mdaziz3289@gmail.com>
2023-05-19 21:02:05 +0600

    in all toys page data shown in tabular form.

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -1,18 +1,61 @@
 import { useLoaderData } from "react-router-dom";
-import ToyCard from "../../components/ToyCard/ToyCard";
 
 
 const AllToys = () => {
 
-    const toys = useLoaderData();
-    return (
-        <div>
-            <h3>{toys.length}</h3>
-            {
-                toys.map(toy=><ToyCard key={toy._id} toy={toy}></ToyCard>)
-            }
-        </div>
-    );
+    const products = useLoaderData();
+
+  return (
+    <div>
+      <table className="min-w-full divide-y divide-gray-200">
+        <thead className="bg-gray-50">
+          <tr>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Name
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Seller
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Sub-category
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Price
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Available Quantity
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Actions
+            </th>
+          </tr>
+        </thead>
+        <tbody className="bg-white divide-y divide-gray-200">
+          {products.map((product) => (
+            <tr key={product.id}>
+              <td className="px-6 py-4 whitespace-nowrap">{product.name}</td>
+              <td className="px-6 py-4 whitespace-nowrap">{product.seller}</td>
+              <td className="px-6 py-4 whitespace-nowrap">
+                {product.subCategory}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap">{product.price}</td>
+              <td className="px-6 py-4 whitespace-nowrap">
+                {product.availableQuantity}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap">
+                <button
+                  className="text-indigo-600 hover:text-indigo-900"
+                  onClick={() => console.log("View Details:", product)}
+                >
+                  View Details
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default AllToys;
